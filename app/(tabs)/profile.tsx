b2d92788c63c3dc475b2bcefc88f032013173edd
robin.boeckmann <robin.boeckmann@flixbus.com>
2024-12-12 22:35:11 +0100

    Add flex code stage 1

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -6,8 +6,8 @@ export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={{}}>Item 1</Text>
-      <Text style={{}}>Item 2</Text>
-      <Text style={{}}>Item 3</Text>
+      <Text style={{alignSelf: 'flex-end'}}>Item 2</Text>
+      <Text style={{fontSize: 40}}>Item 3</Text>
     </View>
   );
 }
@@ -15,5 +15,8 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
         backgroundColor: 'pink',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-start',
       },
 });
