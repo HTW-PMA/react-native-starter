231b404ed2ebf7d19b52710a1eb1db666b60687f
robin.boeckmann <robin.boeckmann@flixbus.com>
2025-01-07 23:47:20 +0100

    Add EAS configuration

--- app/(tabs)/characters/_layout.tsx
+++ app/(tabs)/characters/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router';
 import { View, Text, StyleSheet } from 'react-native';
-import CharactersScreen from '.';
 
 export default function CharactersLayout() {
   return (
